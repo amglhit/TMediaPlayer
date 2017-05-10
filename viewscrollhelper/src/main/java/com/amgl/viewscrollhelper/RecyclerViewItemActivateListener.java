ce7b299065e540e7548ce02c02dda479cccb127c
amgle <amglhit@qq.com>
2017-05-10 18:08:06 +0800

    notify item change on scroll

--- viewscrollhelper/src/main/java/com/amgl/viewscrollhelper/RecyclerViewItemActivateListener.java
+++ viewscrollhelper/src/main/java/com/amgl/viewscrollhelper/RecyclerViewItemActivateListener.java
@@ -34,9 +34,16 @@ public class RecyclerViewItemActivateListener extends RecyclerView.OnScrollListe
         }
     };
 
+    private int mLastFirstVisibleItem = -1;
+
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         mScrollDirectionDetector.onScroll();
+        int firstVisibleItem = mItemPositionHelper.getFirstVisiblePosition();
+        if (firstVisibleItem != mLastFirstVisibleItem) {
+            Timber.d("first visible item changed: %s", firstVisibleItem);
+            mLastFirstVisibleItem = firstVisibleItem;
+        }
     }
 
     private int mActiveIndex = -1;
@@ -49,6 +56,9 @@ public class RecyclerViewItemActivateListener extends RecyclerView.OnScrollListe
         }
     }
 
+    /**
+     * 获取当前激活状态的item，并回调用{@link ItemActivateListener}
+     */
     public void findActiveItem() {
         final ScrollDirectionDetector.ScrollDirection scrollDirection = mScrollDirectionDetector.getScrollDirection();
 
@@ -69,6 +79,13 @@ public class RecyclerViewItemActivateListener extends RecyclerView.OnScrollListe
         }
     }
 
+    /**
+     * 得到需要激活的Item的index。
+     * 根据业务需要覆盖此方法。
+     *
+     * @param scrollDirection
+     * @return
+     */
     protected int findActiveItemIndex(ScrollDirectionDetector.ScrollDirection scrollDirection) {
         return findFirstActiveItemIndex(scrollDirection, mItemPositionHelper);
     }
@@ -79,6 +96,16 @@ public class RecyclerViewItemActivateListener extends RecyclerView.OnScrollListe
         void onDeactivate(int position);
     }
 
+    /**
+     * 根据滚动方向寻找第一个完整显示的Item。
+     * <p>
+     * 向上滚动时从上选择第一个。
+     * 向下滚动时从下选择第一个。
+     *
+     * @param scrollDirection
+     * @param itemPositionHelper
+     * @return
+     */
     private static int findFirstActiveItemIndex(ScrollDirectionDetector.ScrollDirection scrollDirection, IItemPositionHelper itemPositionHelper) {
         final int firstVisibleItem = itemPositionHelper.getFirstVisiblePosition();
         final int lastVisibleItem = itemPositionHelper.getLastVisiblePosition();
@@ -90,14 +117,16 @@ public class RecyclerViewItemActivateListener extends RecyclerView.OnScrollListe
             activeView = itemPositionHelper.getChildAt(activeIndex);
             percent = ItemVisibleHelper.getVisiblePercentage(activeView);
 
-            int index = activeIndex + 1;
-            if (index <= lastVisibleItem) {
-                View itemView = itemPositionHelper.getChildAt(index);
-                int currentPercent = ItemVisibleHelper.getVisiblePercentage(itemView);
-                if (currentPercent > percent) {
-                    activeView = itemView;
-                    activeIndex = index;
-                    percent = currentPercent;
+            if (percent < 80) {
+                int index = activeIndex + 1;
+                if (index <= lastVisibleItem) {
+                    View itemView = itemPositionHelper.getChildAt(index);
+                    int currentPercent = ItemVisibleHelper.getVisiblePercentage(itemView);
+                    if (currentPercent > percent) {
+                        activeView = itemView;
+                        activeIndex = index;
+                        percent = currentPercent;
+                    }
                 }
             }
         } else if (scrollDirection == ScrollDirectionDetector.ScrollDirection.DOWN) {
@@ -105,14 +134,16 @@ public class RecyclerViewItemActivateListener extends RecyclerView.OnScrollListe
             activeView = itemPositionHelper.getChildAt(activeIndex);
             percent = ItemVisibleHelper.getVisiblePercentage(activeView);
 
-            int index = activeIndex - 1;
-            if (index >= firstVisibleItem) {
-                View itemView = itemPositionHelper.getChildAt(index);
-                int currentPercent = ItemVisibleHelper.getVisiblePercentage(itemView);
-                if (currentPercent > percent) {
-                    activeView = itemView;
-                    activeIndex = index;
-                    percent = currentPercent;
+            if (percent < 80) {
+                int index = activeIndex - 1;
+                if (index >= firstVisibleItem) {
+                    View itemView = itemPositionHelper.getChildAt(index);
+                    int currentPercent = ItemVisibleHelper.getVisiblePercentage(itemView);
+                    if (currentPercent > percent) {
+                        activeView = itemView;
+                        activeIndex = index;
+                        percent = currentPercent;
+                    }
                 }
             }
         } else {
